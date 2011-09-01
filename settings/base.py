766fb0371fb5a3d1acca68de678b5fa5be701720
starenka <starenka0@gmail.com>
2011-09-01 16:56:57 +0200

    +analytics

--- settings/base.py
+++ settings/base.py
@@ -4,7 +4,7 @@
 DEBUG = False
 SECRET_KEY = '9387ald3123@#@"""D|||||ASDAS'
 
-GA_CODE = 'xUA-1124837-12'
+GA_CODE = 'UA-1124837-16'
 CACHE_MINUTES = 60*10
 
 DEFAULT_ACCOUNTS = ['starenka','benabraham','kvbik','fczbkk']
